f123554fb23e9e2ce8317ec0f1362870f97704a2
jiewei.ljw <jiewei.ljw@alibaba-inc.com>
2018-02-10 22:09:06 +0800

    new: init a spring-boot app

---
+++ src/test/java/com/jyyz/tech/myfellows/MyFellowsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jyyz.tech.myfellows;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyFellowsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
